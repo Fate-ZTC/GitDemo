08bd0fc0c74a1af35cc13f4357c0783e2ec79376
ZTC <Fate_ZTC@163.com>
2019-08-03 16:58:18 +0800

    增加功能

---
+++ src/com/lqkj/demo/Demo.java
@@ -0,0 +1,16 @@
+package com.lqkj.demo;
+
+/**
+ * @ClassName Demo
+ * @Description TODO
+ * @Author Mr Zhang
+ * @Date 2019/8/3 16:47
+ * @Version 1.0
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("我喜欢你，王雨欣！");
+        System.out.println("哇也喜欢你");
+        System.out.println("真的吗？");
+    }
+}
